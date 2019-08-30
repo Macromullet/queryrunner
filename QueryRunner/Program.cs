befa2ea9ab8e5558f348dab72044e8e65d3ecb29
Brooke Philpott <bphilpott@sentryone.com>
2019-08-30 14:27:21 -0400

    Fixed warnings

--- QueryRunner/Program.cs
+++ QueryRunner/Program.cs
@@ -4,24 +4,14 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace QueryRunner
 {
     class Program
     {
-        private static readonly string _query =
-@"SELECT TOP 1 
-     [member].[member_no],
-     [member].[lastname],
-     [payment].[payment_no],
-     [payment].[payment_dt],
-     [payment].[payment_amt]
-FROM [dbo].[payment]
-INNER JOIN [dbo].[member]
-ON [member].[member_no] = [payment].[member_no];";
-
-        async static Task Main(string[] args)
+        async static Task Main()
         {
             var sqlConnectionStringBuilder = new SqlConnectionStringBuilder
             {
@@ -33,8 +23,7 @@ ON [member].[member_no] = [payment].[member_no];";
             var stopwatch = System.Diagnostics.Stopwatch.StartNew();
 
             var connectionString = sqlConnectionStringBuilder.ToString();
-            //await RunQueryInParallelAsync(connectionString, _query, 100000, 8);
-            await WriteMassiveBatch(connectionString, 1_000_000);
+            await WriteMassiveBatch(connectionString, 1_000_000).ConfigureAwait(false);
 
             stopwatch.Stop();
             var elapsed = stopwatch.Elapsed.TotalSeconds;
@@ -53,11 +42,11 @@ ON [member].[member_no] = [payment].[member_no];";
 
         private static Task WriteMassiveBatch(string connectionString, int rowsToInsert)
         {
-            var dataTable = CreateDataTable();
+            using var dataTable = CreateDataTable();
             for (int i=0;i<rowsToInsert;i++)
             {
-                DataRow dataRow = dataTable.NewRow();
-                var indexString = i.ToString();
+                var dataRow = dataTable.NewRow();
+                var indexString = i.ToString(CultureInfo.InvariantCulture);
                 dataRow[0] = "rowa" + indexString;
                 dataRow[1] = "rowb" + indexString;
                 dataRow[2] = "rowc" + indexString;
@@ -67,62 +56,38 @@ ON [member].[member_no] = [payment].[member_no];";
             {
                 sqlConnection.Open();
                 sqlConnection.Execute("TRUNCATE TABLE TargetTable");
-                using (var sqlTransaction = sqlConnection.BeginTransaction())
+                using var sqlTransaction = sqlConnection.BeginTransaction();
+                using (var sqlDataAdapter = new SqlDataAdapter())
                 {
-                    using (var sqlDataAdapter = new SqlDataAdapter())
-                    {
-                        sqlDataAdapter.UpdateBatchSize = 1000;
-                        using (var sqlCommand = GetSqlCommand(dataTable.TableName))
-                        {
-                            sqlCommand.Connection = sqlConnection;
-                            sqlCommand.Transaction = sqlTransaction;
-                            sqlDataAdapter.InsertCommand = sqlCommand;
-                            sqlDataAdapter.Update(dataTable);
-                            sqlTransaction.Commit();
-                        }
-                    }
-
-                    dataTable.Clear();
+                    sqlDataAdapter.UpdateBatchSize = 1000;
+                    using var sqlCommand = GetSqlCommand(dataTable.TableName);
+                    sqlCommand.Connection = sqlConnection;
+                    sqlCommand.Transaction = sqlTransaction;
+                    sqlDataAdapter.InsertCommand = sqlCommand;
+                    sqlDataAdapter.Update(dataTable);
+                    sqlTransaction.Commit();
                 }
+
+                dataTable.Clear();
             }
 
             return Task.CompletedTask;
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>")]
         private static SqlCommand GetSqlCommand(string tableName)
         {
-            var sqlCommand = new SqlCommand();
-            sqlCommand.CommandText = "Append" + tableName;
-            sqlCommand.CommandType = CommandType.StoredProcedure;
-            sqlCommand.UpdatedRowSource = UpdateRowSource.None;
+            var sqlCommand = new SqlCommand
+            {
+                CommandText = "Append" + tableName,
+                CommandType = CommandType.StoredProcedure,
+                UpdatedRowSource = UpdateRowSource.None
+            };
             sqlCommand.Parameters.Add("@Column1", SqlDbType.NVarChar, 50, "Column1");
             sqlCommand.Parameters.Add("@Column2", SqlDbType.NVarChar, 50, "Column2");
             sqlCommand.Parameters.Add("@Column3", SqlDbType.NVarChar, 50, "Column3");
 
             return sqlCommand;
-        }
-
-        private static async Task RunQueryInParallelAsync(string connectionString, string query, int count, int cores)
-        {
-            var activeTasks = new List<Task>();
-            for (int coreNumber = 0; coreNumber < cores; coreNumber++)
-            {
-                var task = RunQueryAsync(connectionString, query, count);
-                activeTasks.Add(task);
-            }
-
-            await Task.WhenAll(activeTasks.ToArray());
-        }
-
-        private static async Task RunQueryAsync(string connectionString, string query, int count)
-        {
-            using (var dbConnection = new SqlConnection(connectionString))
-            {
-                for (int i = 0; i < count; i++)
-                {
-                    await dbConnection.ExecuteScalarAsync(query);
-                }
-            }
         }
     }
 }
